04d958b2a96740310d5ea21fc3213b21dcdf8d0f
jisoo <ske050588@gmail.com>
2023-01-03 16:31:44 +0900

    기본 프로젝트 생성

---
+++ src/test/java/com/gonyang/diary/DiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonyang.diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
